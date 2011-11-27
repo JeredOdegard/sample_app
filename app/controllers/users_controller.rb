7ffbd9aa0ab1ffeac83e610de16c1a5edd482fb0
Jered Odegard <jered@soromi.com>
2011-11-27 14:18:16 -0800

    Finished signup process and added some form styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,18 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
     @title = "Sign Up"
+  end
+  
+  def create
+    @user = User.new(params[:user]) 
+    if @user.save
+        redirect_to @user
+    else
+        @title = "Sign up"
+        render 'new' 
+    end
   end
   
   def sign_in
